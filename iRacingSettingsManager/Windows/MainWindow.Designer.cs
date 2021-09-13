a896b226c42ac995215e0f30fb72296df3488adc
byBlurr <dbldumont11@gmail.com>
2021-09-13 19:57:15 +0100

    Add project files.

---
+++ iRacingSettingsManager/Windows/MainWindow.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace iRacingSettingsManager.Windows
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewGroupBox = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SaveConfigButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConfigNameBox = new System.Windows.Forms.TextBox();
+            this.ConfigsData = new System.Windows.Forms.DataGridView();
+            this.ConfigName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ConfigFile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.OptionsMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.UpdatesMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ConfigsData)).BeginInit();
+            this.OptionsMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NewGroupBox
+            // 
+            this.NewGroupBox.Controls.Add(this.label2);
+            this.NewGroupBox.Controls.Add(this.SaveConfigButton);
+            this.NewGroupBox.Controls.Add(this.label1);
+            this.NewGroupBox.Controls.Add(this.ConfigNameBox);
+            this.NewGroupBox.Location = new System.Drawing.Point(6, 270);
+            this.NewGroupBox.Name = "NewGroupBox";
+            this.NewGroupBox.Size = new System.Drawing.Size(443, 98);
+            this.NewGroupBox.TabIndex = 1;
+            this.NewGroupBox.TabStop = false;
+            this.NewGroupBox.Text = "Save Current Config";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(6, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Saves your currently active settings.";
+            // 
+            // SaveConfigButton
+            // 
+            this.SaveConfigButton.Location = new System.Drawing.Point(362, 69);
+            this.SaveConfigButton.Name = "SaveConfigButton";
+            this.SaveConfigButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveConfigButton.TabIndex = 3;
+            this.SaveConfigButton.Text = "Save";
+            this.SaveConfigButton.UseVisualStyleBackColor = true;
+            this.SaveConfigButton.Click += new System.EventHandler(this.SaveConfigButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Config Name:";
+            // 
+            // ConfigNameBox
+            // 
+            this.ConfigNameBox.Location = new System.Drawing.Point(152, 23);
+            this.ConfigNameBox.Name = "ConfigNameBox";
+            this.ConfigNameBox.Size = new System.Drawing.Size(285, 23);
+            this.ConfigNameBox.TabIndex = 0;
+            // 
+            // ConfigsData
+            // 
+            this.ConfigsData.AllowUserToAddRows = false;
+            this.ConfigsData.AllowUserToDeleteRows = false;
+            this.ConfigsData.AllowUserToResizeColumns = false;
+            this.ConfigsData.AllowUserToResizeRows = false;
+            this.ConfigsData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ConfigsData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ConfigName,
+            this.ConfigFile});
+            this.ConfigsData.Location = new System.Drawing.Point(12, 43);
+            this.ConfigsData.MultiSelect = false;
+            this.ConfigsData.Name = "ConfigsData";
+            this.ConfigsData.ReadOnly = true;
+            this.ConfigsData.RowTemplate.Height = 25;
+            this.ConfigsData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ConfigsData.Size = new System.Drawing.Size(344, 221);
+            this.ConfigsData.TabIndex = 2;
+            // 
+            // ConfigName
+            // 
+            this.ConfigName.HeaderText = "Name";
+            this.ConfigName.MinimumWidth = 300;
+            this.ConfigName.Name = "ConfigName";
+            this.ConfigName.ReadOnly = true;
+            this.ConfigName.Width = 300;
+            // 
+            // ConfigFile
+            // 
+            this.ConfigFile.HeaderText = "Path";
+            this.ConfigFile.MinimumWidth = 250;
+            this.ConfigFile.Name = "ConfigFile";
+            this.ConfigFile.ReadOnly = true;
+            this.ConfigFile.Visible = false;
+            this.ConfigFile.Width = 250;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(362, 43);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(87, 23);
+            this.LoadButton.TabIndex = 3;
+            this.LoadButton.Text = "Load Config";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(362, 72);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(87, 23);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete Config";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(362, 101);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(87, 23);
+            this.EditButton.TabIndex = 5;
+            this.EditButton.Text = "Edit Config";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // OptionsMenuStrip
+            // 
+            this.OptionsMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UpdatesMenuItem});
+            this.OptionsMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.OptionsMenuStrip.Name = "OptionsMenuStrip";
+            this.OptionsMenuStrip.Size = new System.Drawing.Size(470, 24);
+            this.OptionsMenuStrip.TabIndex = 6;
+            this.OptionsMenuStrip.Text = "Options";
+            this.OptionsMenuStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.OptionsMenuStrip_ItemClicked);
+            // 
+            // UpdatesMenuItem
+            // 
+            this.UpdatesMenuItem.Name = "UpdatesMenuItem";
+            this.UpdatesMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.UpdatesMenuItem.Text = "Check for Updates";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 381);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.ConfigsData);
+            this.Controls.Add(this.NewGroupBox);
+            this.Controls.Add(this.OptionsMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.OptionsMenuStrip;
+            this.Name = "MainWindow";
+            this.Text = "iRacing Settings Manager";
+            this.NewGroupBox.ResumeLayout(false);
+            this.NewGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ConfigsData)).EndInit();
+            this.OptionsMenuStrip.ResumeLayout(false);
+            this.OptionsMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox NewGroupBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SaveConfigButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ConfigNameBox;
+        private System.Windows.Forms.DataGridView ConfigsData;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ConfigName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ConfigFile;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.MenuStrip OptionsMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem UpdatesMenuItem;
+    }
+}
---
+++ iRacingSettingsManager/Windows/MainWindow.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace iRacingSettingsManager.Windows
+{
+    public partial class MainWindow : Form
+    {
+        public static string DownloadLink = "";
+        public static string IRFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "iRacing");
+        public static string IRConfigPath = Path.Combine(IRFolder, "app.ini");
+        public static string SMFolder = Path.Combine(IRFolder, "blurr", "configs");
+        public static string SMConfigPath = Path.Combine(SMFolder, "app");
+
+        private List<IRConfig> Configs = new List<IRConfig>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Make sure our folders exist
+            if (!Directory.Exists(SMFolder)) Directory.CreateDirectory(SMFolder);
+            if (!Directory.Exists(SMConfigPath)) Directory.CreateDirectory(SMConfigPath);
+
+            LoadConfigs();
+        }
+
+        private void LoadConfigs()
+        {
+            // Get list of app.ini files
+            Configs.Clear();
+
+            if (Directory.Exists(SMConfigPath))
+            {
+                foreach (string path in Directory.GetFiles(SMConfigPath))
+                {
+                    if (path.ToLower().EndsWith(".ini"))
+                    {
+                        string configFile = path.Split('\\').Last();
+                        string configName = configFile.Substring(0, configFile.Length - 4);
+                        Configs.Add(new IRConfig(configFile, configName));
+                    }
+                }
+            }
+
+            ConfigsData.ClearSelection();
+            ConfigsData.Rows.Clear();
+
+            foreach (var conf in Configs)
+            {
+                ConfigsData.Rows.Add(conf.Name, conf.File);
+            }
+        }
+
+        private void SaveConfigButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(IRConfigPath))
+            {
+                if (string.IsNullOrEmpty(ConfigNameBox.Text))
+                {
+                    MessageBox.Show("You need to provide a name for the config.", "Error");
+                    return;
+                }
+
+                string newName = ConfigNameBox.Text + ".ini";
+
+                if (File.Exists(Path.Combine(SMConfigPath, newName)))
+                {
+                    MessageBox.Show("A config already exists with this name...", "Error");
+                    return;
+                }
+
+                File.Copy(IRConfigPath, Path.Combine(SMConfigPath, newName));
+                LoadConfigs();
+            }
+            else
+            {
+                MessageBox.Show("Your app.ini can't be found...", "Error");
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            if (ConfigsData.Rows.Count <= 0)
+            {
+                MessageBox.Show("No valid configs to load...", "Error loading config...");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show($"Are you sure you want to load the '{ConfigsData.SelectedRows[0].Cells[0].Value.ToString()}' config? Make sure your current config is saved!", "Load Config?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No) return;
+
+            if (File.Exists(IRConfigPath))
+            {
+                //string path = Path.Combine(SMConfigPath, $"backup-{DateTime.Now.Day}{DateTime.Now.Month}{DateTime.Now.Year}_{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}.ini");
+                //if (File.Exists(path)) File.Delete(path);
+                //File.Copy(IRConfigPath, path);
+
+                LoadConfigs();
+                File.Delete(IRConfigPath);
+            }
+
+            string file = ConfigsData.SelectedRows[0].Cells[1].Value.ToString();
+            File.Copy(Path.Combine(SMConfigPath, file), IRConfigPath);
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (ConfigsData.Rows.Count <= 0)
+            {
+                MessageBox.Show("No valid configs to load...", "Error loading config...");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show($"Are you sure you want to delete the '{ConfigsData.SelectedRows[0].Cells[0].Value.ToString()}' config?", "Delete Config?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No) return;
+
+            string file = ConfigsData.SelectedRows[0].Cells[1].Value.ToString();
+            File.Delete(Path.Combine(SMConfigPath, file));
+            LoadConfigs();
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Functionality has not yet been implemented. You will be able to easily modify your setups!", "Unimplemented Feature");
+            return;
+        }
+
+        private void OptionsMenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            // TODO: Check for updates...
+            MessageBox.Show($"Functionality has not yet been implemented. Check {DownloadLink} for updates for now!", "Unimplemented Feature");
+            return;
+        }
+    }
+
+    class IRConfig
+    {
+        public string File { get; set; }
+        public string Name { get; set; }
+
+        public IRConfig(string file, string name)
+        {
+            File = file;
+            Name = name;
+        }
+    }
+}
